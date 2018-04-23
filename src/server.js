fa2c9f277530d6d9dd599c25cf000bd6f97ba086
Adam Szmyd <szmydadam@gmail.com>
2018-04-23 10:04:38 +0200

    Basic websocket

--- src/server.js
+++ src/server.js
@@ -4,5 +4,12 @@ const app = express();
 
 app.use(express.static(path.join(__dirname, 'public')));
 
+require('express-ws')(app);
+
 app.listen(8100, () => console.log('Example app listening on port 8100!'));
 
+app.ws('/echo', function(ws, req) {
+    ws.on('message', function(msg) {
+        ws.send(msg);
+    });
+});
